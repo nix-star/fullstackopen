23dc4c521e4a03ee96ae8bbf01d496a0421e10b7
Nicolás Chiesa <nicochiesa01@gmail.com>
2026-08-19 18:24:39 -0300

    Part 3.1-3.6

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,53 @@
+let persons = require('./persons-mock.js')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  person ? response.json(person) : response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  if (!person.name || !person.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.find(p => p.name === person.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  person.id = Math.floor(Math.random() * 1000000).toString()
+  console.log(person)
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
